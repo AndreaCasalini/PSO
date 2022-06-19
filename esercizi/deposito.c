f5a2d98fb95270d62935d60cf34c5b659987b383
AndreaCasalini <AndreaCasalini>
2022-06-19 15:46:48 +0200

    completato deposito

--- esercizi/deposito.c
+++ esercizi/deposito.c
@@ -4,17 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-/*variabili globali*/
 
+#define V 2  /*numero di vani*/
+#define N 4  /*numero bagagli per vano*/
+
+/*variabili globali*/
 /* semaforo di mutua esclusione per l'accesso a tutte le variabili condivise 
 (simula il semaforo di mutua esclusiome associato ad una istanza di tipo monitor) */
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
 pthread_cond_t coda;
-
-
-#define V 7    /*numero di vani*/
-#define N 4  /*numero bagagli per vano*/
-
+int counter=0;/*contatore di utenti in coda in attesa di risveglio*/
 int deposito[V]={0};/*array struttura deposito*/
 
 int to_store(int bagagli_utente){ /*ritorno il vano in cui mettiamo i bagagli*/
@@ -23,21 +22,26 @@ int to_store(int bagagli_utente){ /*ritorno il vano in cui mettiamo i bagagli*/
           for(int i=0;i<V;i++){
                if(deposito[i]+bagagli_utente<=N){
                     deposito[i]+=bagagli_utente;
+                    printf("INSERISCO BAGAGLIO NEL VANO %d e sono il thread con id %lu\n",i,pthread_self());
+                    pthread_mutex_unlock(&mutex);
                     return i;
                }
-               /*non ho trovato un vano libero per i bagagli quindi devo attendere*/
-               pthread_cond_wait(&coda, &mutex);
           }
+          /*non ho trovato un vano libero per i bagagli quindi devo attendere*/
+          counter++;
+          printf("-->sono [%lu] e sono bloccato\n",pthread_self());
+          pthread_cond_wait(&coda, &mutex);
+          counter--;
      }
-     pthread_mutex_unlock(&mutex);
 }
 
 void to_retire(int vano_utente, int bagagli_utente){
      pthread_mutex_lock(&mutex);
 
      deposito[vano_utente]-=bagagli_utente;
-     pthread_cond_signal(&coda);
-
+     for(int i=0;i<counter;i++)
+          pthread_cond_signal(&coda);
+     
      pthread_mutex_unlock(&mutex);
 }
 
@@ -50,20 +54,21 @@ void *user(void*id){
           printf("Problemi con l'allocazione di ptr\n");
           exit(-1);
      }
-     srand(time(NULL));/*inizializzo il seme*/
      /*attribuisco un numero casuale di bagagli ad ogni utente compreso tra 1 e N*/
-     int bagagli_utente= (rand() % (N-1)) + 1 ; 
+     int bagagli_utente= (rand() % (N)) + 1 ; 
      int i=0;
      while(1){
           /*deposito bagaglio*/
-          printf("Utente-[Thread%d e identificatore %lu] ENTRO NEL DEPOSITO PER LASCIARE I [%d] BAGAGLI\n", *pi, pthread_self(),bagagli_utente);
+          printf("Utente-[Thread%d e identificatore %lu] ENTRO CON N.[%d] BAGAGLI (iter. %d)\n", *pi, pthread_self(),bagagli_utente,i);
           int n_vano = to_store(bagagli_utente);
           /*aspetto*/
-          printf("Utente-[Thread%d e identificatore %lu] STO ASPETTANDO PER POI RITIRARE (iter. %d)\n", *pi, pthread_self(), i);
-          sleep(6);
+          printf("Utente-[Thread%d e identificatore %lu] ASPETTO (iter. %d)\n", *pi, pthread_self(), i);
+          sleep(5);
           /*ritiro bagaglio*/
-          printf("Utente-[Thread%d e identificatore %lu] ESCO DAL DEPOSITO (iter. %d)\n", *pi, pthread_self(), i);
+          printf("Utente-[Thread%d e identificatore %lu] ESCO  (iter. %d)\n", *pi, pthread_self(), i);
           to_retire(n_vano,bagagli_utente);
+          i++;
+          sleep(2);
      }
       
 }
@@ -76,6 +81,7 @@ int main (int argc,char **argv)
      int *p;
      int NUM_THREADS = 10;
 
+     srand(time(NULL));/*inizializzo il seme*/
 
      thread=(pthread_t *) malloc(NUM_THREADS * sizeof(pthread_t));
      if (thread == NULL)
@@ -107,6 +113,6 @@ int main (int argc,char **argv)
           ris= *p;
           printf("Pthread %d-esimo restituisce %d\n", i, ris);
      }
-
+     
      exit(0);
 }
